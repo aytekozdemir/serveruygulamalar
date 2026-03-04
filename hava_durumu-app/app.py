e34d4bba50f94b99d9170d440e907dfc9383b727
aytekozdemir <aytekozdemir@articncmakine>
2026-03-04 17:02:47 +0300

    app.py gercek kodla degistirildi

--- hava_durumu-app/app.py
+++ hava_durumu-app/app.py
@@ -1,1 +1,18 @@
-# ...existing code from d:\hava_durumu\app.py...
+Writing objects: 100% (45/45), 3.75 KiB | 640.00 KiB/s, done.
+Total 45 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/aytekozdemir/serveruygulamalar.git
+ * [new branch]      main -> main
+branch 'main' set up to track 'origin/main'.
+
+D:\githubserver>git add .
+
+D:\githubserver>git commit -m "app.py icerigi gercek kodla degistirildi"
+On branch main
+Your branch is up to date with 'origin/main'.
+
+nothing to commit, working tree clean
+
+D:\githubserver>git push
+Everything up-to-date
+
+D:\githubserver>
